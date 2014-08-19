8b3025b6f5f3448f9ef838f79b1076a2141c64c6
shiv sharma <shiv_softengg@yahoo.com>
2014-08-19 13:34:24 +0100

    First refreshtag.py

--- letsbuildit/tagging/refreshtag.py
+++ letsbuildit/tagging/refreshtag.py
@@ -1,38 +1,58 @@
-from sh import git , cd 
 from tempfile import *
-from os import *
+import os
+from git import *
+import re
 from tagging import models
 
 def addtagsindb(components):
+        print("I am in")	
+	tagndate = []
 	if components == "all":
-		'''do this'''
+		# Create a temp directory to download git repos
 		try:
-			tempdir = tempdir = mkstemp(prefix="tagging_",dir="/tmp") 
-			temp = tempdir[1]
+			tempdir = mkdtemp(prefix="tagging_",dir="/tmp") 
 		except:
 			raise
 
-		cd(temp)
-		componentlist = models.components.objects.all()
-		for component in componentlist:
-			if not os.path.exists(temp/component):
-				try:
-					os.makedirs(temp/component)
-				except:
-					raise
-			if component.vc == "git":
-				vcs  = models.vc_credentials.objects.filter(version_control='git')
-				user = vcs[1]
-				passwd = vcs[2]
-					
-			cd(temp/component)
-			repo_credentials = component.repo[:8] + user + ':' + passwd + '@' + component[8:]
-			try:
-				repo = git.clone(repo_credentials)
-			except:
-				raise
-			 
-		
-	else:
-		'''do that'''
+	# Change to that directory
 
+	os.chdir(tempdir)
+	# Get the list of all components and its attributes 
+
+	componentlist = models.components.objects.all()
+	for component in componentlist:
+	
+        # Get Version control credentials 
+
+		if component.vc == "git":
+			vcs  = models.vc_credentials.objects.filter(version_control='git')
+			user = vcs[1]
+			passwd = vcs[2]
+					
+		repo_credentials = component.repo[:8] + user + ':' + passwd + '@' + component[8:]
+		try:
+			repo = git.clone(repo_credentials)
+		except:
+			raise
+	
+	# Chagne to the repo directory 
+			
+		os.chdir(component)
+		repo_path = temp + '/' + component 	
+				
+		repo = Repo(repo_path)
+		git = repo.git
+			
+		taglist = git.for_each_ref(sort="refname", format='%(refname) - %(taggerdate)')
+		regstring = re.compile('refs/tags/*')
+
+		for t in taglist.rsplit("\n"):
+			if regstring.match(t):
+				tempstr = re.sub('\+[0-9]+','',re.sub(regstring,'',t))
+				tagdate[tempstr.split("-")[0]] = tempstr.split("-")[1]
+				
+		print(tagdate)
+
+if __name__ == '__main__':
+	print("i m in main")
+	addtagsindb("all")
